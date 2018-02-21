c245cb2f41f42cbcb3fd97fb4825ca6ab7b9361f
Cristian G Guerrero <guyik.cgg@gmail.com>
2018-02-21 16:41:02 +0100

    Add simple example using web-services

---
+++ web-services/ejemplo1/ejemplo.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+    <form action="ejemplo.php" method="get">
+        <label for="rango">Rango</label>
+        <input type="range" min="3" max="10" step="1" name="rango" value="5">
+        <input type="submit" value="Magic Square!">
+    </form>
+
+    <table style="text-align: center; border: solid 1px">
+        <?php
+        // Required by Google App Engine
+        libxml_disable_entity_loader(false);
+
+        // Load Magic Square service
+        $wsdl = "http://www.cs.fsu.edu/~engelen/magic.wsdl";
+
+        $client = new SoapClient($wsdl);
+
+        $rango = $_GET["rango"];
+
+        echo "<p>Rango = ".$rango."</p>";
+        $elementos = $client->magic($rango);
+
+        for ($i=0; $i < $rango; $i++) {
+            echo "<tr>";
+            for ($j=0; $j < $rango; $j++) {
+                echo '<td style="padding: 5px">';
+                print($elementos[$i][$j]);
+                echo "</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
