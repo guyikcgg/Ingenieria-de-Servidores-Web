5e2f5f020e08e8e89d7e33f01830a52c187b3066
Cristian G Guerrero <guyik.cgg@gmail.com>
2018-03-17 00:36:52 +0100

    Have web service working

--- web-services/ejemplo2/client.php
+++ web-services/ejemplo2/client.php
@@ -6,7 +6,7 @@
 </head>
 <body>
 	<form action="client.php" method="get">
-		<label for="qtt">Numero<br></label>
+		<label for="qtt">Numero:<br></label>
 		<input type="number" name="qtt" required>
 		<input type="submit" value="Multiplicar">
 	</form>
@@ -15,27 +15,21 @@
 // Required by Google App Engine
 libxml_disable_entity_loader(false);
 
-// Load Magic Square service
-$location = "http://localhost:8000/server.php/";
+// Load custom service
+$location = "http://localhost:8080/server.php";
 
+// Set Soap timeout
 ini_set('default_socket_timeout', 5);
 
 $client = new SoapClient(null, array('location'=>$location, 'uri'=>"http://test-uri/", 'trace'=>1));
 
-print_r($client->__getFunctions());
-echo $client->__getFunctions();
 $qtt = $_GET["qtt"];
-echo $qtt;
-
-$ret = $client->hola();
-//$ret = $client->multiplica_por_2(6);
-echo $ret;
 ?>
 
 <p>
-<!--?php
-// echo "2 * ".$qtt." = ".$client->multiplica_por_2($qtt);
-?-->
+<?php
+echo "2 * ".$qtt." = ".$client->multiplica_por_2($qtt);
+?>
 </p>
 
 </body>
--- web-services/ejemplo2/server.php
+++ web-services/ejemplo2/server.php
@@ -2,23 +2,19 @@
 function multiplica_por_2($numero) {
 	return 2*$numero;
 }
-function hola() {
-	return "hola";
-}
 
 // Required by Google App Engine
 libxml_disable_entity_loader(false);
 
 ini_set('soap.wsdl_cache_enabled', 0);
 ini_set('soap.wsdl_cache_ttl', 0);
-ini_set('default_socket_timeout', 5);
 
 try {
-	$server = new SoapServer (null, array('uri' => "http://test-uri/"));
+	$server = new SoapServer (null, array('uri' => "http://localhost/"));
 	$server->addFunction("multiplica_por_2");
-	$server->addFunction("hola");
 	$server->handle();
 } catch (SOAPFault $f) {
 	print $f->faultstring;
 }
+
 ?>
