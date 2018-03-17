a0ae7312019c9ea88d9c19c2bdb84c59484b4864
Cristian G Guerrero <guyik.cgg@gmail.com>
2018-03-17 20:25:39 +0100

    client.php: add authentication using OAuth 2.0

--- web-services/contador-visitas/client.php
+++ web-services/contador-visitas/client.php
@@ -3,8 +3,43 @@
 <head>
 	<meta charset="utf-8">
 	<title></title>
+	<meta name="google-signin-client_id" content="503312404598-np6muhtp10pok057ggbou3l5oihdkm1v.apps.googleusercontent.com">
+	<script src="https://apis.google.com/js/platform.js" async defer></script>
 </head>
 <body>
+	<div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+	<script>
+	function onSignIn(googleUser) {
+		var profile = googleUser.getBasicProfile();
+		var id_token = googleUser.getAuthResponse().id_token;
+
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', 'https://my-visit-counter.appspot.com/auth.php');
+		xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		xhr.onload = function() {
+			console.log('Signed in as: ' + xhr.responseText);
+		};
+		xhr.send('idtoken=' + id_token);
+
+		// Log from client-side
+		console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+		console.log('Name: ' + profile.getName());
+		console.log('Image URL: ' + profile.getImageUrl());
+		console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	}
+	</script>
+
+	<a href="#" onclick="signOut();">Sign out</a>
+	<script>
+	function signOut() {
+		var auth2 = gapi.auth2.getAuthInstance();
+		auth2.signOut().then(function () {
+				console.log('User signed out.');
+				});
+	}
+	</script>
+
 	<form action="client.php" method="get">
 		<label for="qtt">Numero:<br></label>
 		<input type="number" name="qtt" required>
