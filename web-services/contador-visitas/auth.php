24ed4d636b386021e7185d6d9618755025d1f260
Cristian G Guerrero <guyik.cgg@gmail.com>
2018-03-20 19:28:22 +0100

    auth.php: fix indentation

--- web-services/contador-visitas/auth.php
+++ web-services/contador-visitas/auth.php
@@ -8,12 +8,12 @@ $id_token = $_POST["idtoken"];
 $client = new Google_Client(['client_id' => $CLIENT_ID]);  // Specify the CLIENT_ID of the app that accesses the backend
 $payload = $client->verifyIdToken($id_token);
 if ($payload) {
-  $userid = $payload['sub'];
-  // If request specified a G Suite domain:
-  //$domain = $payload['hd'];
-  echo "USER ID (SERVER SIDE): " . $userid;
+    $userid = $payload['sub'];
+    // If request specified a G Suite domain:
+    //$domain = $payload['hd'];
+    echo "USER ID (SERVER SIDE): " . $userid;
 } else {
-  // Invalid ID token
-  echo "INVALID ID TOKEN";
+    // Invalid ID token
+    echo "INVALID ID TOKEN";
 }
 ?>
